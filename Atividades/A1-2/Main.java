46adbbfdfe7901f996740ac2dc32139473264af7
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2024-09-09 22:56:38 -0300

    Add Atividade 2

--- Atividades/A1-2/Main.java
+++ Atividades/A1-2/Main.java
@@ -1,7 +1,21 @@
+/*
+2. Crie uma classe Computador com os seguintes atributos:  tipo (String), marca (String), memoriaRAM (int) e armazenamento (int), todos os valores em GB.
 
+Crie os métodos:
+exibirTipoComputador() - este método deve exibir o tipo do computador (Notebook, Desktop, All-in-one) no padrão abaixo:
+
+"Tipo do computador: Notebook"
+
+exibirConfiguracoes() -  este método deve exibir a marca, a quantidade de memória RAM e o armazenamento do computador no padrão abaixo:
+
+"Marca: HP, Memória RAM: 16GB, Armazenamento: 256GB"
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ola");
+        
+        Computador computador = new Computador("Notebook", "HP", 16, 256);
+        computador.exibirTipoComputador();
+        computador.exibirConfiguracoes();
     }
 }
 
